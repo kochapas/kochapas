4299215dee592ed25d895d2c08c83964de1acf8f
kochapas <kochapas@outlook.com>
2021-09-10 00:48:55 +0900

    Add image link to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ chef_project.secondary_role = "Full-Stack Developer"
 chef_project.project_link = "https://thankyouchef.herokuapp.com/"
 chef_project.repository_link = "https://github.com/mewband12/thankyouchef"
 chef_project.youtube_link = ""
+chef_project.img_link = "https://res.cloudinary.com/k0ch176as/image/upload/v1631202385/Screenshot_2021-09-10_004547_g9f463.jpg"
 chef_project.save
 
 barc_project = Project.new()
@@ -29,7 +30,7 @@ barc_project.primary_role = "Project Manager"
 barc_project.secondary_role = "Full-Stack Developer"
 barc_project.project_link = "https://www.barcsnap.com/"
 barc_project.repository_link = "https://github.com/2tonepantone/barcsnap"
-barc_project.youtube_link = ""
+barc_project.img_link = "https://res.cloudinary.com/k0ch176as/image/upload/v1631202385/Screenshot_2021-09-10_004459_mbiwq1.jpg"
 barc_project.save
 
 puts "🎊 Seed process done! #{Project.count} projects seeded to the database..."
