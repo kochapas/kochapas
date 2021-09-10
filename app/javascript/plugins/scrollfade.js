75f4f1005f9d0f751653e69d33f815023200bd0f
kochapas <kochapas@outlook.com>
2021-09-10 15:39:07 +0900

    Move fade active algorithm to separated method.

--- app/javascript/plugins/scrollfade.js
+++ app/javascript/plugins/scrollfade.js
@@ -1,24 +1,29 @@
 const scrollfade = () => {
+  updateActive();
   $(document).on("scroll", () =>
     {
-      const pageTop = $(document).scrollTop();
-      const pageBottom = pageTop + $(window).height();
-      var tags = $(".scroll-fade");
-      for (var i = 0; i < tags.length; i++)
-      {
-        let tag = tags[i];
-        if ($(tag).position().top + $(tag).height() - 80 < pageTop) {
-          $(tag).removeClass("active");
-        }
-        else if ($(tag).position().top + 80 < pageBottom) {
-          $(tag).addClass("active");
-        }
-        else {
-          $(tag).removeClass("active");
-        }
-      }
+      updateActive();
     }
   );
 };
+
+const updateActive = () => {
+  const pageTop = $(document).scrollTop();
+  const pageBottom = pageTop + $(window).height();
+  var tags = $(".scroll-fade");
+  for (var i = 0; i < tags.length; i++)
+  {
+    let tag = tags[i];
+    if ($(tag).position().top + $(tag).height() - 80 < pageTop) {
+      $(tag).removeClass("active");
+    }
+    else if ($(tag).position().top + 80 < pageBottom) {
+      $(tag).addClass("active");
+    }
+    else {
+      $(tag).removeClass("active");
+    }
+  }
+}
 
 export { scrollfade };
